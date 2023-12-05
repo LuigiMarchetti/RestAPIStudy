a15558d167aca6732c735207ace8a2c93e00ea7c
Luigi Marchetti <Luigi.Marchetti@philips.com>
2023-12-05 16:33:33 -0300

    minor adj

--- Spring boot study/src/main/java/com/study/luigi/student/utils/StudentConstants.java
+++ Spring boot study/src/main/java/com/study/luigi/student/utils/StudentConstants.java
@@ -3,7 +3,6 @@ package com.study.luigi.student.utils;
 public class StudentConstants {
     public static final String STUDENTNOTFOUND = "Student was not found";
     public static final String MANDOTORYEMAIL = "Email is mandatory";
-
     public static final String EMAILALREADYREGISTERED = "This email is already registered";
 
 }
